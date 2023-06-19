8fa3bff2c32bc6b922b7ae5cb4f664104f48f329
Shane Butler <shanedbutler@gmail.com>
2023-06-19 13:18:42 -0400

    Initialize new library component

--- projects/pup-ui/src/lib/pup-ui.module.ts
+++ projects/pup-ui/src/lib/pup-ui.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { PupUiComponent } from './pup-ui.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
 
 
 
 @NgModule({
   declarations: [
-    PupUiComponent
+    PupUiComponent,
+    NavBarComponent
   ],
   imports: [
   ],
